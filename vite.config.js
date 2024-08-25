dabeb012a66c8081fef70ff07e6bef18fb6b58d2
Francisco-Escobar99 <escobarfrancisco9905@gmail.com>
2024-08-24 21:42:01 -0600

    configuracion dist

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'src', // Aquí especificas la carpeta de salida deseada
+    outDir: 'dist', // Aquí especificas la carpeta de salida deseada
   },
 })
